44e03dca91f6871c239aec2b535bcfb714cf360c
leaf-lxh <meleaf@qq.com>
2024-09-19 22:23:28 +0800

    feat: 添加nkid字段

--- service-video/src/main/java/com/inkneko/nekowindow/video/vo/VideoPostBriefVO.java
+++ service-video/src/main/java/com/inkneko/nekowindow/video/vo/VideoPostBriefVO.java
@@ -11,7 +11,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class VideoPostBriefVO {
-
+    private Long nkid;
     private String title;
     private String description;
     private UploadUserVO uploader;
