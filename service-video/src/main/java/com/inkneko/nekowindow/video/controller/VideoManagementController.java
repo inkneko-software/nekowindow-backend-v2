2b1b9a111ef0ee77a76b98621e2f378d91e9d772
leaf-lxh <meleaf@qq.com>
2024-09-21 15:36:14 +0800

    feat: 实现视频基础信息更新，实现用户已上传视频查询

--- service-video/src/main/java/com/inkneko/nekowindow/video/controller/VideoManagementController.java
+++ service-video/src/main/java/com/inkneko/nekowindow/video/controller/VideoManagementController.java
@@ -1,17 +1,40 @@
 package com.inkneko.nekowindow.video.controller;
 
 import com.inkneko.nekowindow.common.Response;
+import com.inkneko.nekowindow.common.util.GatewayAuthUtils;
+import com.inkneko.nekowindow.video.dto.UpdatePostBriefDTO;
+import com.inkneko.nekowindow.video.entity.VideoPost;
+import com.inkneko.nekowindow.video.service.VideoService;
+import com.inkneko.nekowindow.video.vo.UserUploadedVideoStatisticsVO;
 import io.swagger.v3.oas.annotations.Operation;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
-@RequestMapping(path = "/api/v1/videoManagement")
+@RequestMapping(path = "/api/v1/video/management")
 public class VideoManagementController {
+    VideoService videoService;
+
+    public VideoManagementController(VideoService videoService) {
+        this.videoService = videoService;
+    }
+
     @GetMapping("/getUploadedVideos")
     @Operation(summary = "获取已上传视频列表")
-    public Response<?> getUploadedVideos(){
-        return null;
+    public Response<List<UserUploadedVideoStatisticsVO>> getUploadedVideos(@RequestParam(defaultValue = "1") Long page, @RequestParam(defaultValue = "20") Long size){
+        Long uid = GatewayAuthUtils.auth();
+        List<VideoPost> userVideoPosts = videoService.getUploadedVideoPosts(uid, page, size);
+        List<UserUploadedVideoStatisticsVO> convertedVos = userVideoPosts.stream().map(UserUploadedVideoStatisticsVO::new).toList();
+        return new Response<>("ok", convertedVos);
+    }
+
+    @PostMapping("/updateVideoPost")
+    @Operation(summary = "更新已上传的视频信息")
+    public Response<?> updateVideoPost(@RequestBody UpdatePostBriefDTO dto){
+        Long uid = GatewayAuthUtils.auth();
+        videoService.updatePostBrief(dto, uid);
+        return new Response<>("ok");
     }
 }
