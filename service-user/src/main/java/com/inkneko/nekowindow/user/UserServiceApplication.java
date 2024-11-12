014cc383e060c9819f84b3d2ed4236e12f989016
leaf-lxh <meleaf@qq.com>
2024-11-12 23:45:23 +0800

    feat: 添加FeignGatewayAuthConfig与ServiceExceptionHandler

--- service-user/src/main/java/com/inkneko/nekowindow/user/UserServiceApplication.java
+++ service-user/src/main/java/com/inkneko/nekowindow/user/UserServiceApplication.java
@@ -1,16 +1,20 @@
 package com.inkneko.nekowindow.user;
 
+import com.inkneko.nekowindow.autoconfig.api.FeignGatewayAuthConfig;
+import com.inkneko.nekowindow.autoconfig.controller.ServiceExceptionHandler;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableFeignClients(basePackages = "com.inkneko.nekowindow.api")
 @OpenAPIDefinition(info = @Info(title = "service-user", description = "用户服务API接口说明", version = "0.0.1"))
 @EnableAsync
+@Import({FeignGatewayAuthConfig.class, ServiceExceptionHandler.class})
 public class UserServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
