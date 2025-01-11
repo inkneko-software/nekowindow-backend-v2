06e18621ed4bc2643f0f861f585843d9eb03bb5b
leaf-lxh <meleaf@qq.com>
2025-01-11 16:16:33 +0800

    feat: 定义队列与交换机及其相关绑定

--- service-encode/src/main/java/com/inkneko/nekowindow/encode/config/EncodeConfig.java
+++ service-encode/src/main/java/com/inkneko/nekowindow/encode/config/EncodeConfig.java
@@ -1,19 +1,30 @@
 package com.inkneko.nekowindow.encode.config;
 
+import lombok.Data;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Data
 @Configuration
 public class EncodeConfig {
-    public static final String encodeQueueName = "nekowindow-encode-queue";
-    public static final String topicExchangeName = "nekowindow-encode-exchange";
+    public static final String NK_TOPIC_EXCHANGE_NAME = "heimusic-topic-exchange";
+    public static final String NK_PROBE_QUEUE_NAME = "nekowindow-probe-queue";
+    public static final String NK_ENCODE_VIDEO_QUEUE_NAME = "nekowindow-encode-video-queue";
+    public static final String NK_ENCODE_AUDIO_QUEUE_NAME = "nekowindow-encode-audio-queue";
+    public static final String NK_CONCAT_QUEUE_NAME = "nekowindow-concat-queue";
 
+    /**
+     * GOP（关键帧）大小，默认为5秒
+     */
+    @Value("${encode.gop-size:5}")
+    private int gopSize;
 
     @Bean
     public Jackson2JsonMessageConverter jsonMessageConverter() {
@@ -21,18 +32,48 @@ public class EncodeConfig {
     }
 
     @Bean
-    Queue queue(){
-        return new Queue(encodeQueueName, true);
-    }
-
-    @Bean
     TopicExchange topicExchange(){
-        return new TopicExchange(topicExchangeName);
+        return new TopicExchange(NK_TOPIC_EXCHANGE_NAME);
     }
 
     @Bean
-    Binding encodeBinding(Queue queue, TopicExchange topicExchange) {
-        return BindingBuilder.bind(queue).to(topicExchange).with("encode.file_id.#");
+    Queue probeQueue(){
+        return new Queue(NK_PROBE_QUEUE_NAME, true);
+    }
+
+    @Bean
+    Queue encodeVideoQueue(){
+        return new Queue(NK_ENCODE_VIDEO_QUEUE_NAME, true);
+    }
+
+    @Bean
+    Queue encodeAudioQueue(){
+        return new Queue(NK_ENCODE_AUDIO_QUEUE_NAME, true);
+    }
+
+    @Bean
+    Queue concatQueue(){
+        return new Queue(NK_CONCAT_QUEUE_NAME, true);
+    }
+
+    @Bean
+    Binding probeBinding(@Qualifier("probeQueue") Queue queue, TopicExchange topicExchange) {
+        return BindingBuilder.bind(queue).to(topicExchange).with("probe");
+    }
+
+    @Bean
+    Binding encodeVideoBinding(@Qualifier("encodeVideoQueue") Queue queue, TopicExchange topicExchange) {
+        return BindingBuilder.bind(queue).to(topicExchange).with("encode.video");
+    }
+
+    @Bean
+    Binding encodeAudioBinding(@Qualifier("encodeAudioQueue") Queue queue, TopicExchange topicExchange) {
+        return BindingBuilder.bind(queue).to(topicExchange).with("encode.audio");
+    }
+
+    @Bean
+    Binding concatBinding(@Qualifier("concatQueue") Queue queue, TopicExchange topicExchange) {
+        return BindingBuilder.bind(queue).to(topicExchange).with("concat");
     }
 
 }
