ddca7abe809b29044093f4dc3b97cedc4594bf52
leaf-lxh <meleaf@qq.com>
2025-02-25 23:26:25 +0800

    fix: 代码格式化

--- service-video/src/main/java/com/inkneko/nekowindow/video/VideoServiceApplication.java
+++ service-video/src/main/java/com/inkneko/nekowindow/video/VideoServiceApplication.java
@@ -4,6 +4,7 @@ import com.inkneko.nekowindow.autoconfig.api.FeignGatewayAuthConfig;
 import com.inkneko.nekowindow.autoconfig.controller.ServiceExceptionHandler;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -12,7 +13,7 @@ import org.springframework.context.annotation.Import;
 @SpringBootApplication
 @EnableFeignClients(basePackages = "com.inkneko.nekowindow.api")
 @Import({FeignGatewayAuthConfig.class, ServiceExceptionHandler.class})
-@OpenAPIDefinition(info = @Info(title = "service-video", description = "视频服务API接口说明" , version = "0.0.1"))
+@OpenAPIDefinition(info = @Info(title = "service-video", description = "视频服务API接口说明", version = "0.0.1"))
 public class VideoServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(VideoServiceApplication.class, args);
