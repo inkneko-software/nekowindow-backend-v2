973a16b2aef257c12d5a1fbfdef5eaf5ce10b710
leaf-lxh <meleaf@qq.com>
2025-02-25 23:29:31 +0800

    refactor: 更新接口信息

--- api/api-encode/src/main/java/com/inkneko/nekowindow/api/encode/client/EncodeFeignClient.java
+++ api/api-encode/src/main/java/com/inkneko/nekowindow/api/encode/client/EncodeFeignClient.java
@@ -2,10 +2,11 @@ package com.inkneko.nekowindow.api.encode.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient("nekowindow-service-encode")
 public interface EncodeFeignClient {
 
     @PostMapping("/internal/encode/parseSourceVideo")
-    void parseSourceVideo(String url);
+    void parseSourceVideo(@RequestParam Long videoId, @RequestParam String sourceVideoUrl);
 }
