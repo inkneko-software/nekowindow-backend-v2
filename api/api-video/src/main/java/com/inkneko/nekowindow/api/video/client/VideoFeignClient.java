c4e901b97b21422061feb18e4317cca7f9d08528
leaf-lxh <meleaf@qq.com>
2025-02-25 23:29:52 +0800

    feat: 添加视频转码状态更新接口

--- api/api-video/src/main/java/com/inkneko/nekowindow/api/video/client/VideoFeignClient.java
+++ api/api-video/src/main/java/com/inkneko/nekowindow/api/video/client/VideoFeignClient.java
@@ -1,11 +1,18 @@
 package com.inkneko.nekowindow.api.video.client;
 
+import com.inkneko.nekowindow.api.video.dto.UpdateVideoResourceConversionStateDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient("nekowindow-service-video")
 public interface VideoFeignClient {
 
     @GetMapping("/internal/video/updateVideoResource")
-    void updateVideoResource(Long videoId, Long dashMpdUrl);
+    void updateVideoResource(@RequestParam Long videoId, @RequestParam Long dashMpdUrl);
+
+    @PostMapping("/internal/video/updateVideoResourceConversionState")
+    void updateVideoResourceConversionState(UpdateVideoResourceConversionStateDTO dto);
+
 }
