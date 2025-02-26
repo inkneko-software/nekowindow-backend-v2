ef62073ca0509ef3ef598d56eaf48779edceb78d
leaf-lxh <meleaf@qq.com>
2025-02-26 23:01:42 +0800

    feat: 实现API包中定义的内部接口

--- service-video/src/main/java/com/inkneko/nekowindow/video/feign/VideoInternalController.java
+++ service-video/src/main/java/com/inkneko/nekowindow/video/feign/VideoInternalController.java
@@ -2,14 +2,29 @@ package com.inkneko.nekowindow.video.feign;
 
 
 import com.inkneko.nekowindow.api.video.client.VideoFeignClient;
+import com.inkneko.nekowindow.api.video.dto.UpdateVideoResourceConversionStateDTO;
+import com.inkneko.nekowindow.video.entity.VideoPostResource;
+import com.inkneko.nekowindow.video.service.VideoService;
 import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class VideoInternalController implements VideoFeignClient {
+
+    @Autowired
+    VideoService videoService;
+
     @Operation(summary = "更新视频资源信息")
     @Override
     public void updateVideoResource(Long videoId, Long dashMpdUrl) {
 
+    }
+
+    @Override
+    public void updateVideoResourceConversionState(UpdateVideoResourceConversionStateDTO dto) {
+        videoService.updateVideoPostResourceConversionState(dto);
     }
 }
