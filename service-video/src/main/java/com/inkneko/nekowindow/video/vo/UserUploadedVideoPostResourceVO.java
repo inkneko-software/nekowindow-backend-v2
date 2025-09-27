351eaac1d9244432bcb753d9b9e7f47554204bf8
leaf-lxh <meleaf@qq.com>
2025-09-27 09:49:40 +0800

    feat: 添加管理侧视频资源VO

---
+++ service-video/src/main/java/com/inkneko/nekowindow/video/vo/UserUploadedVideoPostResourceVO.java
@@ -0,0 +1,33 @@
+package com.inkneko.nekowindow.video.vo;
+
+import com.inkneko.nekowindow.video.entity.VideoPostResource;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserUploadedVideoPostResourceVO {
+    Long videoId;
+    Integer duration;
+    Integer state;
+    String reviewFailedReason;
+    Integer conversionState;
+    String conversionFailedReason;
+    String dashMpdUrl;
+    LocalDateTime createdAt;
+
+    public UserUploadedVideoPostResourceVO(VideoPostResource videoPostResource){
+        this.videoId = videoPostResource.getVideoId();
+        this.duration = videoPostResource.getDuration();
+        this.state = videoPostResource.getState();
+        this.reviewFailedReason = videoPostResource.getReviewFailedReason();
+        this.conversionState = videoPostResource.getConvertState();
+        this.conversionFailedReason = videoPostResource.getConvertErrMsg();
+        this.dashMpdUrl = videoPostResource.getDashMpdUrl();
+        this.createdAt = videoPostResource.getCreatedAt();
+    }
+}
