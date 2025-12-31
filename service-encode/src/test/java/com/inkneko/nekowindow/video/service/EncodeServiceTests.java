c1400e47321ee61db7a88271d4a77545be30efe7
leaf-lxh <meleaf@qq.com>
2025-12-31 15:57:49 +0800

    test: 添加测试

---
+++ service-encode/src/test/java/com/inkneko/nekowindow/video/service/EncodeServiceTests.java
@@ -0,0 +1,8 @@
+package com.inkneko.nekowindow.video.service;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EncodeServiceTests {
+
+}
---
+++ service-video/src/test/java/com/inkneko/nekowindow/video/service/impl/VideoServiceImplTest.java
@@ -0,0 +1,15 @@
+package com.inkneko.nekowindow.video.service.impl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class VideoServiceImplTest {
+
+    @Test
+    void createVideoPost() {
+
+    }
+}
