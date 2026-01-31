610c11f4ac7c320e0b1226624365929c6eb91730
leaf-lxh <meleaf@qq.com>
2026-01-31 22:19:25 +0800

    chore: 移除测试用固定nameServer

--- service-user/src/main/java/com/inkneko/nekowindow/user/mq/template/VideoCoinExtRocketMQTemplate.java
+++ service-user/src/main/java/com/inkneko/nekowindow/user/mq/template/VideoCoinExtRocketMQTemplate.java
@@ -4,6 +4,6 @@ import org.apache.rocketmq.spring.annotation.ExtRocketMQTemplateConfiguration;
 import org.apache.rocketmq.spring.core.RocketMQTemplate;
 import org.springframework.stereotype.Component;
 
-@ExtRocketMQTemplateConfiguration(nameServer = "10.200.0.10:9876", group = "video-coin-tx-producer-group", tlsEnable =  "false")
+@ExtRocketMQTemplateConfiguration(group = "video-coin-tx-producer-group", tlsEnable =  "false")
 public class VideoCoinExtRocketMQTemplate extends RocketMQTemplate{
 }
