bbf938799a6608d954f3fd6ef267df157ce32785
leaf-lxh <meleaf@qq.com>
2026-01-31 22:22:53 +0800

    feat: 更新视频硬币数

--- service-video/src/main/java/com/inkneko/nekowindow/video/mq/consumer/VideoCoinConsumer.java
+++ service-video/src/main/java/com/inkneko/nekowindow/video/mq/consumer/VideoCoinConsumer.java
@@ -1,11 +1,15 @@
 package com.inkneko.nekowindow.video.mq.consumer;
 
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.inkneko.nekowindow.api.mq.PostVideoCoinDTO;
 import com.inkneko.nekowindow.video.entity.VideoCoinRecord;
+import com.inkneko.nekowindow.video.entity.VideoPost;
 import com.inkneko.nekowindow.video.mapper.VideoCoinRecordMapper;
+import com.inkneko.nekowindow.video.mapper.VideoPostMapper;
 import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
 import org.apache.rocketmq.spring.core.RocketMQListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RocketMQMessageListener(
@@ -15,12 +19,18 @@ import org.springframework.stereotype.Component;
 public class VideoCoinConsumer implements RocketMQListener<PostVideoCoinDTO> {
 
     private final VideoCoinRecordMapper mapper;
+    private final VideoPostMapper videoPostMapper;
 
-    public VideoCoinConsumer(VideoCoinRecordMapper mapper) {
+    public VideoCoinConsumer(
+            VideoCoinRecordMapper mapper,
+            VideoPostMapper videoPostMapper
+            ) {
         this.mapper = mapper;
+        this.videoPostMapper = videoPostMapper;
     }
 
     @Override
+    @Transactional
     public void onMessage(PostVideoCoinDTO dto) {
         VideoCoinRecord record = new VideoCoinRecord();
         record.setUid(dto.getUserId());
@@ -28,5 +38,12 @@ public class VideoCoinConsumer implements RocketMQListener<PostVideoCoinDTO> {
         record.setNum(dto.getCoinNum());
         record.setOrderId(dto.getOrderId());
         mapper.insert(record);
+
+        videoPostMapper.update(
+                null,
+                Wrappers.<VideoPost>lambdaUpdate()
+                        .eq(VideoPost::getNkid, dto.getNkid())
+                        .setSql("coin = coin + " + dto.getCoinNum())
+        );
     }
 }
