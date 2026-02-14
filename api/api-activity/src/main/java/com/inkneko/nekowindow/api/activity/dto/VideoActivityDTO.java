5a92df161369d92535e76e4a75ee34decf93ec2a
leaf-lxh <meleaf@qq.com>
2026-02-14 14:05:53 +0800

    feat: 动态服务API

---
+++ api/api-activity/src/main/java/com/inkneko/nekowindow/api/activity/dto/VideoActivityDTO.java
@@ -0,0 +1,13 @@
+package com.inkneko.nekowindow.api.activity.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class VideoActivityDTO {
+    private Long nkid;
+    private Long userId;
+}
